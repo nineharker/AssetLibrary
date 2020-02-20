06506c176027048be94e32bcd01db0654e839fe3
nineharker <nineharker45@gmail.com>
2020-02-21 00:46:54 +0900

    delete imagekit

--- AssetLibrary/settings.py
+++ AssetLibrary/settings.py
@@ -15,10 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# imageFieldを扱うのに必要
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
